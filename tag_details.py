8f756794237c6d712f9e4d7e0c2158f6cd1659a8
Dheepak <sslvgdek@gmail.com>
2017-10-24 22:03:30 -0500

    Able to fetch details of all mp3 files

--- tag_details.py
+++ tag_details.py
@@ -1,30 +1,50 @@
-
 def fetch_tag_details(song_location,song_name,tag_output_file,delimiter):
     import mutagen
 
     song_file = song_location+song_name
     song_tags = mutagen.File(song_file)
 
-    album = song_tags['TALB'].text[0]
-    artist = song_tags['TPE1'].text[0]
-    track_name = song_tags['TCOM'].text[0]
-    year = song_tags['TDRC'].text[0]
-    genre = song_tags['TCON'].text[0]
-    track_number = song_tags['TRCK'].text[0]
-    title = song_tags['TIT2'].text[0]
-    song_writer = song_tags['TEXT'].text[0]
+    try:
+        album = song_tags['TALB'].text[0]
+    except KeyError:
+        album = 'error'
+    try:
+        artist = song_tags['TPE1'].text[0]
+    except KeyError :
+        artist = 'error'
+    try:
+        track_name = song_tags['TCOM'].text[0]
+    except KeyError :
+        track_name = 'error'
+    try:
+        year = song_tags['TDRC'].text[0]
+    except KeyError :
+        year = 'error'
+    try:
+        genre = song_tags['TCON'].text[0]
+    except KeyError :
+        genre = 'error'
+    try:
+        track_number = song_tags['TRCK'].text[0]
+    except KeyError :
+        track_number = 'error'
+    try:
+        title = song_tags['TIT2'].text[0]
+    except KeyError:
+        title = 'error'
+    try:
+        song_writer = song_tags['TEXT'].text[0]
+    except KeyError :
+        song_writer = 'error'
     length = song_tags.info.length
     length_mins = int(length / 60)
     length_secs = length % 60
-    length_human_read = str(length_mins) + ':' + str(length_secs)[:2] + ' mins'
+    length_human_read = str(length_mins) + ':' + str(length_secs)[:2]
 
-    header_data = 'album' + delimiter + 'artist' + delimiter + 'track_name' + delimiter + 'year' + delimiter + 'genre' + delimiter + \
-                  'track_number' + delimiter + 'title' + delimiter + 'song_writer' + delimiter + 'length'
-
-    tag_data = album + delimiter + artist + delimiter + track_name + delimiter + str(
+    tag_data = song_name+delimiter+album + delimiter + artist + delimiter + track_name + delimiter + str(
         year) + delimiter + genre + delimiter + track_number + \
                delimiter + title + delimiter + song_writer + delimiter + str(length_human_read)
 
     f = open(tag_output_file,'a')
     f.write(tag_data + '\n')
-    f.close
+    f.flush()
--- tag_header.py
+++ tag_header.py
@@ -1,7 +1,6 @@
 def fetch_tag_header(song_location,song_name,tag_output_file,delimiter):
-    song_file = song_location+song_name
-    header_data = 'album' + delimiter + 'artist' + delimiter + 'track_name' + delimiter + 'year' + delimiter + 'genre' + delimiter + \
-                  'track_number' + delimiter + 'title' + delimiter + 'song_writer' + delimiter + 'length'
+    header_data = 'file_name'+ delimiter+ 'album' + delimiter + 'artist' + delimiter + 'track_name' + delimiter + 'year' + delimiter + 'genre' + delimiter + \
+                  'track_number' + delimiter + 'title' + delimiter + 'song_writer' + delimiter + 'length(mins)'
     f = open(tag_output_file, 'w')
     f.write(header_data + '\n')
     f.close
